466722de0ee4a161177364ce552a0a0d75d13001
ahmad shabrawy <ahmad.shabrawy95@gmail.com>
2026-02-25 06:30:09 +0200

    update param to name

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -67,9 +67,9 @@ class ModelTrainer:
                 # There are other ways to use the Model Registry, which depends on the use case,
                 # please refer to the doc for more information:
                 # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-                mlflow.sklearn.log_model(best_model, "model", registered_model_name=best_model_name)
+                mlflow.sklearn.log_model(best_model, name="model", registered_model_name=best_model_name)
             else:
-                mlflow.sklearn.log_model(best_model, "model")
+                mlflow.sklearn.log_model(best_model, name="model")
 
 
         
@@ -139,7 +139,7 @@ class ModelTrainer:
         os.makedirs(model_dir_path,exist_ok=True)
 
         Network_Model=NetworkModel(preprocessor=preprocessor,model=best_model)
-        save_object(self.model_trainer_config.trained_model_file_path,obj=NetworkModel)
+        save_object(self.model_trainer_config.trained_model_file_path,obj=Network_Model)
         #model pusher
         save_object("final_model/model.pkl",best_model)
         
